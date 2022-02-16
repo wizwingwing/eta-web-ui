3c8a6228a3c4f01a2d80f499600f7d35afaf5a65
Yodnam <wichanannam2@gmail.com>
2022-02-16 16:10:28 +0700

    Create Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import Login from "./Component/Page/Login"
 import Register from "./Component/Page/Register"
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./Component/Page/home";
+import Dashboard from "./Component/Page/Dashboard/Dashboard";
+import Navbar from "./Component/Fragement/Navbar";
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/navbar" element={<Navbar />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
--- src/Component/Page/Login.tsx
+++ src/Component/Page/Login.tsx
@@ -2,13 +2,12 @@ import { CheckboxRemember, FormLabel, TextFieldBase } from '../Element/CustomMat
 import { TextHeaderH1 } from '../Element/Fontsize.element'
 import { BoxContent, BoxFooter, BoxLogin, ButtonLogin, Container, LinkForgotPassword, LinkSignUp } from '../Element/Login.element'
 import { BiLogInCircle } from 'react-icons/bi';
-import { useRef, useState } from 'react';
-import axios from 'axios';
+import { useRef} from 'react';
+// import axios from 'axios';
 
 const Login = () => {
 
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+
   const homeRef = useRef<HTMLAnchorElement>(null)
 
   return (
@@ -24,7 +23,7 @@ const Login = () => {
           <LinkForgotPassword href="#" >Forgot Password</LinkForgotPassword>
         </BoxFooter>
         <ButtonLogin><BiLogInCircle style={{ marginRight: "0.5rem" }} />Login</ButtonLogin>
-        <LinkSignUp ref={homeRef} style={{ display: "none" }} to="/" />
+        <LinkSignUp ref={homeRef} style={{ display: "none" }} to="/home" />
         <BoxFooter style={{ display: "flex", justifyContent: "center" }} margin={"0 0 1rem 0"}>
           <LinkSignUp to="/register" >You don't have an account? Signup!</LinkSignUp>
         </BoxFooter>
